66cebe0b593ef356863e946719a506bcdfdb25e6
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-22 11:19:48 -0600

    Use main() function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("This is the text", TextType.BOLD_TEXT, "www.gitlab.com")
+    print(text_node)
+
 if __name__ == '__main__':
-    print("hello world")
+    main()
 
