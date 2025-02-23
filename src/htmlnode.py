aa56d549aa546eee449bf354312442d9b68554a3
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-23 11:49:29 -0600

    Add ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from functools import reduce
 
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
@@ -7,7 +7,7 @@ class HTMLNode():
         self.children = children
         self.props = props
 
-    def to_html(self):
+    def to_html(self) -> str:
         raise NotImplemented
 
     def props_to_html(self):
@@ -22,7 +22,7 @@ class HTMLNode():
         return f"tag: {self.tag}; value: {self.value}; children: {self.children}; props: {self.props}"
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag=tag, value=value, children=None, props=props)
 
     def to_html(self):
@@ -31,5 +31,17 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return f"{self.value}"
         return f"<{self.tag}{self.props_to_html() if self.props else ""}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("HTML Tag is required")
+        if not self.children:
+            raise ValueError("Children nodes are required")
+        children_value = reduce(lambda html_str, node: html_str + node.to_html(), self.children, "")
+        return f"<{self.tag}{self.props_to_html() if self.props else ""}>{children_value}</{self.tag}>"
 
 
