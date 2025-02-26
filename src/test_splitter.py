e79695dd6a6fc6e6f66c9f032f98121276525981
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-25 18:46:04 -0600

    Test methods must start with test

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -3,7 +3,7 @@ from mdsplitter import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
-    def code1(self):
+    def test_code1(self):
         expected = [
             TextNode("This is text with a ", TextType.NORMAL_TEXT),
             TextNode("code block", TextType.CODE),
@@ -12,7 +12,6 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TextType.NORMAL_TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(new_nodes, expected)
-        self.assertTrue(False)
 
 if __name__ == '__main__':
     _ = unittest.main()
