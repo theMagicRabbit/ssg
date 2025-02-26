6ce2d3c267f227fb5144ea9f2d7182d942061eb5
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-25 18:42:20 -0600

    Add unit test for splitter

---
+++ src/test_splitter.py
@@ -0,0 +1,20 @@
+import unittest
+from mdsplitter import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def code1(self):
+        expected = [
+            TextNode("This is text with a ", TextType.NORMAL_TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.NORMAL_TEXT),
+        ]
+        node = TextNode("This is text with a `code block` word", TextType.NORMAL_TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, expected)
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    _ = unittest.main()
+
+
