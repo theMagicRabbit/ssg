66b3cb4df8bd496542eb4d502392e743c829aae4
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-27 19:06:12 -0600

    Improve tests
    Markdown image title text is set off by quotes not apostrophe. Add test
    4 to validate that dashes are matched in urls.

--- src/test_extractor.py
+++ src/test_extractor.py
@@ -3,20 +3,51 @@ from mdextractor import extract_markdown_images
 
 class TestTextNode(unittest.TestCase):
     def test_images1(self):
-        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+                ]
+        text = (
+                "This is text with a "
+                "![rick roll](https://i.imgur.com/aKaOqIh.gif) "
+                "and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+                )
         images = extract_markdown_images(text)
         self.assertEqual(images, expected)
 
     def test_images2(self):
-        expected = [("image", "https://www.github.com/image.jpg"), ("image2", "https://i.imgur.com/fJRm4Vk.jpeg"), ("image3", "http://example.com/unsecure.png")]
-        text = "This is text with a ![image](https://www.github.com/image.jpg) and ![image2](https://i.imgur.com/fJRm4Vk.jpeg) and another ![image3](http://example.com/unsecure.png)"
+        expected = [
+                ("image", "https://www.github.com/image.jpg"),
+                ("image2", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                ("image3", "http://example.com/unsecure.png")
+                ]
+        text = (
+                "This is text with a "
+                "![image](https://www.github.com/image.jpg) "
+                "and ![image2](https://i.imgur.com/fJRm4Vk.jpeg) "
+                "and another ![image3](http://example.com/unsecure.png)"
+                )
         images = extract_markdown_images(text)
         self.assertEqual(images, expected)
 
     def test_images3(self):
         expected = [("boot dev logo", "https://www.boot.dev/img/bootdev-logo-full-small.web")]
-        text = "This is text with a ![boot dev logo](https://www.boot.dev/img/bootdev-logo-full-small.web 'boot dev logo')"
+        text = "This is text with a ![boot dev logo](https://www.boot.dev/img/bootdev-logo-full-small.web \"boot dev logo\")"
+        images = extract_markdown_images(text)
+        self.assertEqual(images, expected)
+
+    def test_images4(self):
+        expected = [
+                ("image", "https://www.web_site.com/image.jpg"),
+                ("image2", "https://web-site.com/fJRm4Vk.jpeg"),
+                ("image3", "http://example-web_site.com/unsecure.png")
+                ]
+        text = (
+                "This is text with a "
+                "![image](https://www.web_site.com/image.jpg) "
+                "and ![image2](https://web-site.com/fJRm4Vk.jpeg) "
+                "and another ![image3](http://example-web_site.com/unsecure.png)"
+                )
         images = extract_markdown_images(text)
         self.assertEqual(images, expected)
 
