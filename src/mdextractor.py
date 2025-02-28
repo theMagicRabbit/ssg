95988b5816aaa98ff883853ff7ffa0aac7e9ea20
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-02-27 19:50:12 -0600

    Add extractor for links

--- src/mdextractor.py
+++ src/mdextractor.py
@@ -1,10 +1,16 @@
 import re
 
-ALT_RE = r"!\[(?P<alt>[\w\s]+)\]"
-SRC_RE = r"\((?P<url>https{,1}[-\w:./]+)(?:\s*\".*\"){,1}\)"
-IMAGE_RE = ALT_RE + SRC_RE
-
 def extract_markdown_images(md_text):
+    ALT_RE = r"!\[(?P<alt>[\w\s]+)\]"
+    SRC_RE = r"\((?P<src>https{,1}[-\w:./]+)(?:\s*\".*\"){,1}\)"
+    IMAGE_RE = ALT_RE + SRC_RE
     matches = re.findall(IMAGE_RE, md_text)
+    return matches
+
+def extract_markdown_links(md_text):
+    LINK_TEXT_RE = r"(?<!!)\[(?P<txt>[^\[\]]+)\]"
+    URL_RE = r"\((?P<url>[^\(\)]*)\)"
+    MD_LINK_RE = LINK_TEXT_RE + URL_RE
+    matches = re.findall(MD_LINK_RE, md_text)
     return matches
 
