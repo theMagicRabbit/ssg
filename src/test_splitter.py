b4c94d7950e89a1119159a6d8f0c4e136e52a411
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-01 12:36:56 -0600

    Add intial test for images

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -1,5 +1,5 @@
 import unittest
-from mdsplitter import split_nodes_delimiter
+from mdsplitter import split_nodes_delimiter, split_nodes_image
 from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
@@ -25,13 +25,27 @@ class TestTextNode(unittest.TestCase):
 
     def test_italic1(self):
         expected = [
-            TextNode("This is text with a ", TextType.NORMAL_TEXT),
-            TextNode("italic phrase", TextType.ITALIC_TEXT),
-            TextNode(" word", TextType.NORMAL_TEXT),
-        ]
+                TextNode("This is text with a ", TextType.NORMAL_TEXT),
+                TextNode("italic phrase", TextType.ITALIC_TEXT),
+                TextNode(" word", TextType.NORMAL_TEXT),
+                ]
         node = TextNode("This is text with a _italic phrase_ word", TextType.NORMAL_TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC_TEXT)
         self.assertEqual(new_nodes, expected)
+
+    def test_split_images(self):
+        expected = [
+                TextNode("This is text with an ", TextType.NORMAL_TEXT),
+                TextNode("image", TextType.IMAGES, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.NORMAL_TEXT),
+                TextNode("second image", TextType.IMAGES, "https://i.imgur.com/3elNhQu.png"),
+                ]
+        node = TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                TextType.NORMAL_TEXT,
+                )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(expected, new_nodes)
 
 if __name__ == '__main__':
     _ = unittest.main()
