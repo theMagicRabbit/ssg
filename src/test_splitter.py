1f684f5a6064bebb574bbb508a8d0ccdd06858af
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-01 13:42:30 -0600

    More tests for images

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -33,7 +33,7 @@ class TestTextNode(unittest.TestCase):
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC_TEXT)
         self.assertEqual(new_nodes, expected)
 
-    def test_split_images(self):
+    def test_split_images1(self):
         expected = [
                 TextNode("This is text with an ", TextType.NORMAL_TEXT),
                 TextNode("image", TextType.IMAGES, "https://i.imgur.com/zjjcJKZ.png"),
@@ -42,6 +42,51 @@ class TestTextNode(unittest.TestCase):
                 ]
         node = TextNode(
                 "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                TextType.NORMAL_TEXT,
+                )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(expected, new_nodes)
+
+    def test_split_images2(self):
+        expected = [
+                TextNode("This string has no images, but it does have **bold** text.", TextType.NORMAL_TEXT),
+                ]
+        node = TextNode(
+                "This string has no images, but it does have **bold** text.",
+                TextType.NORMAL_TEXT,
+                )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(expected, new_nodes)
+
+    def test_split_images3(self):
+        expected = [
+                TextNode("This string has no images, but it does have **bold** text.", TextType.NORMAL_TEXT),
+                TextNode("This string has ", TextType.NORMAL_TEXT),
+                TextNode("an image", TextType.IMAGES, "http://example.com/image.png"),
+                ]
+        node = TextNode(
+                "This string has no images, but it does have **bold** text.",
+                TextType.NORMAL_TEXT,
+                )
+        node2 = TextNode(
+                "This string has ![an image](http://example.com/image.png)",
+                TextType.NORMAL_TEXT,
+                )
+        new_nodes = split_nodes_image([node, node2])
+        self.assertListEqual(expected, new_nodes)
+
+    def test_split_images4(self):
+        expected = []
+        node = TextNode(None, None, None)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(expected, new_nodes)
+
+    def test_split_images5(self):
+        expected = [
+                TextNode("an image", TextType.IMAGES, "http://example.com/image.png"),
+                ]
+        node = TextNode(
+                "![an image](http://example.com/image.png)",
                 TextType.NORMAL_TEXT,
                 )
         new_nodes = split_nodes_image([node])
