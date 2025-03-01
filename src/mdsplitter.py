6372702fea2ab0c2e6446f3d27c405a2a27888a8
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-01 13:02:38 -0600

    Add image splitter

--- src/mdsplitter.py
+++ src/mdsplitter.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from mdextractor import extract_markdown_links, extract_markdown_images
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -11,7 +12,23 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             match_text_type = map(lambda enum_tup: (enum_tup[1], node.text_type) if not enum_tup[0] % 2 else (enum_tup[1], text_type), enum_strs)
             new_nodes_map = map(lambda str_type_tup: TextNode(str_type_tup[0], str_type_tup[1]), match_text_type)
             new_nodes.extend(new_nodes_map)
+    return new_nodes
 
-
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        node_text = node.text
+        if not node.text_type == TextType.NORMAL_TEXT:
+            new_nodes.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        for alt,src in images:
+            text_splits = node_text.split(f"![{alt}]({src})", 1)
+            new_nodes.append(TextNode(text_splits[0], TextType.NORMAL_TEXT))
+            node_text = text_splits[1]
+            new_nodes.append(TextNode(alt, TextType.IMAGES, src))
     return new_nodes
 
