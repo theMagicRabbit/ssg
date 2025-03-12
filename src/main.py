123fdd0a361ae669da529849a09fcab2b47619c9
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-11 20:09:28 -0500

    Copy static files to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from textnode import TextNode, TextType
+from os.path import exists, isfile, join
+from os import mkdir, listdir
+from shutil import rmtree, copy
+
+def recursive_cp_dir(src, dest):
+    if not exists(src):
+        raise FileNotFoundError(f"{src} does not exist")
+    if exists(dest):
+        rmtree(dest)
+    mkdir(dest)
+
+    for f in listdir(src):
+        rel_src = join(src, f)
+        if not isfile(rel_src):
+            rel_dest = join(dest, f)
+            recursive_cp_dir(rel_src, rel_dest)
+        else:
+            print(f"Copy {rel_src} to {dest}")
+            copy(rel_src, dest)
+
 
 def main():
-    text_node = TextNode("This is the text", TextType.BOLD_TEXT, "www.gitlab.com")
-    print(text_node)
+    recursive_cp_dir("static", "public")
 
 if __name__ == '__main__':
     main()
