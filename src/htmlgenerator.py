d6ebba06e92f72391e2461e72c52b775f61f1e16
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-12 19:27:02 -0500

    Generate and write html files

---
+++ src/htmlgenerator.py
@@ -0,0 +1,25 @@
+from textconverter import markdown_to_html_node
+from mdextractor import extract_title
+from os.path import basename, join
+from os import mkdirs
+
+def generate_page(from_path, template_path, dest_path):
+    with open(from_path, 'r', encoding='UTF-8') as source_file:
+        markdown_source = source_file.read()
+    with open(template_path, 'r', encoding='UTF-8') as template_file:
+        template_source = template_file.read()
+    html_div = markdown_to_html_node(markdown_source).to_html()
+    title = extract_title(markdown_source)
+    html_doc = template_source.replace("{{ Title }}", title).replace("{{ Content }}", html_div)
+    file_slug = splitext(basename(from_path))
+    mkdirs(dest_path, exist_ok=True)
+    html_file = join(dest_path, f"{file_slug}.html")
+    with open(html_file, 'w', encoding='UTF-8') as out_file:
+        out_file.write(html_doc)
+
+    
+    
+
+
+    
+
