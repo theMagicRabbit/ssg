e69a6fb4317f0caed164d6d814345248508b9c0e
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-12 21:33:06 -0500

    Run webserver for rendered html

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 ~/.local/share/virtualenvs/ssg-3xlCM2Xs/bin/python3 ~/Documents/src/github/ssg/src/main.py
+cd public && ~/.local/share/virtualenvs/ssg-3xlCM2Xs/bin/python3 -m http.server 8888
 
