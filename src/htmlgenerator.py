997a2745838e611b3d90cca2ec29bd45781277df
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-03-12 19:36:42 -0500

    Fix typos and missing imports

--- src/htmlgenerator.py
+++ src/htmlgenerator.py
@@ -1,7 +1,7 @@
 from textconverter import markdown_to_html_node
 from mdextractor import extract_title
-from os.path import basename, join
-from os import mkdirs
+from os.path import basename, join, splitext
+from os import makedirs
 
 def generate_page(from_path, template_path, dest_path):
     with open(from_path, 'r', encoding='UTF-8') as source_file:
@@ -12,7 +12,7 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_source)
     html_doc = template_source.replace("{{ Title }}", title).replace("{{ Content }}", html_div)
     file_slug = splitext(basename(from_path))
-    mkdirs(dest_path, exist_ok=True)
+    makedirs(dest_path, exist_ok=True)
     html_file = join(dest_path, f"{file_slug}.html")
     with open(html_file, 'w', encoding='UTF-8') as out_file:
         out_file.write(html_doc)
